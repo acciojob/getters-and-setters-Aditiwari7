3f3e239266edde24f6cbe2bb6ca9e36f48c248e9
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-04-07 19:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 //complete this code
 class Person {
-	constructor(name, age){
-		this._name = name;
-		this._age = age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	get name(){
-		return this._name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	set age(){
-		return this._age;
-	}
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`);
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach() {
-		console.log(`${this.name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
-// Example
-const person = new Person('John', 25);
+// Example usage
+const person = new Person("John", 25);
 console.log(person.name); // "John"
 person.age = 30;
 
