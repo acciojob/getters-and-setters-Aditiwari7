fb8d6849950f1d3f4e712ad4f0653fb8c38a1150
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-04-07 19:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,6 +26,18 @@ class Teacher extends Person {
 	}
 }
 
+const person = new Person("John", 25);
+console.log(person.name); // "John"
+person.age = 30;
+
+const student = new Student("Jane", 20);
+console.log(student.name); // "Jane"
+student.study(); // "Jane is studying"
+
+const teacher = new Teacher("Bob", 35);
+console.log(teacher.name); // "Bob"
+teacher.teach(); // "Bob is teaching"
+
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
